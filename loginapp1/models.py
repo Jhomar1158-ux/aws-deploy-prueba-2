712cb6a6487c33a19432cdcff3266aba80864fad
Jhomar <jhomarcristianelias@gmail.com>
2021-12-03 07:02:29 -0500

    Actualizando Home

---
+++ loginapp1/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class User1(models.Model):
+    first_name = models.CharField(max_length = 60)
+    last_name = models.CharField(max_length = 60)
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 150)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField( max_length=255)
+    release_date = models.DateField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Book ID: ({self.id})| Title: {self.title}| Network: {self.network}| Release Date: {self.release_date}| Description: {self.description} ||"
